7cbd0eeb210b74d080a179557db57d1fdb35db06
execute1606 <72761350+execute1606@users.noreply.github.com>
2021-06-06 21:33:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,28 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int cbinsearch(int* arr, int size, int value) {
+    int count = 0, min = 0, max = size - 1, ave = 0;
+    while (min <= max) {
+        mid = (min + max) / 2;
+        if (arr[ave] == value) {
+            count++;
+            break;
+        }
+        value < arr[ave] ? max = ave - 1 : min = ave + 1;
+    }
+    int l = 1;
+    bool boo1 = true, boo2 = true;
+    if (count) {
+        while (boo1 || boo2) {
+            if (ave + l < size) {
+                arr[ave + l] == value ? count++ : boo1 = false;
+            }
+            if (ave - l >= 0) {
+                arr[ave - l] == value ? count++ : boo2 = false;
+            }
+            if (ave + l >= size && ave - l < 0) break;
+            l++;
+        }
+    }
+    return count;
 }
